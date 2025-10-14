2800a7e64383aacba0d15f6de474b22ace437376
felipesilva2102 <felipefrancelino2015@gmail.com>
2025-10-14 16:33:32 -0300

    ajustes health

---
+++ src/main/java/com/fstech/app/controller/HealthController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fstech.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author Felipe
+ */
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
+
