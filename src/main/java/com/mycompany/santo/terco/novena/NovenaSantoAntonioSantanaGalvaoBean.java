999364e8b09cf688f40e414f073d6e741f2a501b
felipesilva2102 <felipefrancelino2015@gmail.com>
2025-10-18 21:26:01 -0300

    adição de mais novenas e ajustes para organização em pastas.

---
+++ src/main/java/com/mycompany/santo/terco/novena/NovenaSantoAntonioSantanaGalvaoBean.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.santo.terco.novena;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Felipe
+ */
+@Named
+@SessionScoped
+@Getter
+@Setter
+public class NovenaSantoAntonioSantanaGalvaoBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String quemFoi = "São Frei Galvão é o primeiro santo que nasceu em terras brasileiras e seu testemunho de virtudes deve ser seguido por nós, que um dia desejamos alcançar o céu.\n"
+            + "\n"
+            + "Pertencente a uma família nobre, ele não se apegou aos bens materiais. Ao contrário: dedicou sua vida à oração e à caridade.\n"
+            + "\n"
+            + "Tamanha era a fé de Santo Antônio de Sant’Anna Galvão que vários fenômenos místicos, como bilocação e levitação, acontecerem com ele ainda em vida.\n"
+            + "\n"
+            + "Em cada um dos dias da novena, você irá meditar sobre a história de um homem que amou verdadeiramente a Deus e mostrou que a santidade é um chamado para o povo do Brasil.";
+
+    private String oracaoInicial = "Santíssima Trindade, Pai, Filho e Espírito Santo, eu Vos adoro, louvo e dou graças pelos benefícios que me fizestes. Peço-Vos, por tudo que fizeste e sofreste o Vosso servo Frei Galvão, que aumenteis em mim a fé, a esperança e a caridade, e Vos dignastes conceder-me a graça que ardentemente almejo. Amém.";
+
+    private String oracaoFinal = "Faça agora seu pedido à Deus pela intercessão de Antônio de Santana Galvão.\n"
+            + "\n"
+            + "Pai Nosso, Ave Maria e Glória ao Pai.\n"
+            + "\n"
+            + "São Frei Galvão, Deus fez em ti maravilhas e, por meio de ti, anunciou o Evangelho do amor, do acolhimento e da misericórdia para com os mais fracos e sofredores. Com o coração agradecido à nação brasileira por tão grande dom, nós te pedimos: intercede por nós junto a Deus, para que possamos vivenciar, na comunidade eclesial, os valores evangélicos que, de modo tão heroico, viveste. Dá-nos a coragem e a perseverança na fé e abertura ao Espírito Santo de Deus, para que possamos ser sal da terra e luz do mundo. Amém.";
+
+    private String primeiroDia = "Frei Galvão foi o primeiro santo brasileiro. Foi Canonizado pelo Papa Bento XVI, durante sua visita ao Brasil, em 11 de maio de 2007. As pílulas de Frei Galvão, pequeninos papeis, escritos com versículos da Bíblia, dobrados e ingeridos pelos fieis, realizaram vários milagres em seus seguidores. Frei Galvão nasceu em Guaratinguetá, no interior do Estado de S. Paulo. Filho de Antônio Galvão de França, Capitão-mor, pertencia a Ordem Terceira de São Francisco e a Ordem do Carmo. Se dedicava ao comércio e era conhecido pela sua particular generosidade. A mãe, Isabel Leite de Barros, teve onze filhos e morreu com apenas 38 anos de idade com fama de grande caridosa.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó admirável Santo Antônio de Santana Galvão, primeiro Santo desta terra de Santa Cruz, dai-nos a graça de perseverarmos em nossos propósitos de santidade, imitando o vosso exemplo para que nunca mais ofendamos a Deus por pensamentos, palavras ou obras.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String segundoDia = "Frei Galvão viveu com seus irmãos numa casa grande e rica, seus pais gozavam de prestígio social e influência política. Viviam num ambiente profundamente religioso. Com a idade de 13 anos, foi para Belém, na Bahia a fim de estudar no Seminário dos Padres Jesuítas, onde já se encontrava seu irmão José. Permaneceu de 1752 a 1756, com notáveis progressos no estudo e na prática da vida cristã. Seu pai, preocupado com as ações do Marques do Pombal, contra os jesuítas, aconselhou o Frei a viver com os Frades Menores Descalços de São Pedro de Alcântara, do Convento em Taubaté, próximo a Guaratinguetá.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó bondoso Santo Antônio de Santana Galvão, santo da paz e do bem, dai-nos a graça de vivermos uma vida sem conflitos, imitando o vosso exemplo para fazermos já aqui nesta terra um mundo de paz.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String terceiroDia = "Aos 21 anos, no dia 15 de abril de 1760, Frei Galvão ingressou no noviciado do Convento de São Boaventura, na Vila de Macacu, no Rio de Janeiro. Durante o noviciado distinguiu-se pela piedade e pela prática das virtudes, conforme consta no livro “Religiosos Brasileiros”. No dia 16 de abril de 1761, fez o juramento dos Franciscanos, de se empenhar na defesa da Imaculada Conceição de Nossa Senhora, doutrina ainda controvertida, mas aceita e defendida pela Ordem Franciscana. Um ano depois foi admitido à ordenação sacerdotal, em 11 de julho de 1762. Depois de ordenado foi mandado para o Convento de S. Francisco em São Paulo, com o fim de aperfeiçoar os estudos de filosofia e teologia como também de exercitar-se no apostolado.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó amável Santo Antônio de Santana Galvão, fiel imitador de São Francisco, dai-nos a graça de amarmos a Deus acima de toda criatura, imitando o vosso exemplo para que não nos deixemos influenciar por teorias ecologistas que diminuem a importante do homem perante a criação.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String quartoDia = "A maturidade espiritual de Frei Galvão teve sua expressão máxima na “entrega a Maria” como o seu “filho e escravo perpétuo”. Terminado os estudos, em 1768, foi nomeado Pregador, Confessor dos Leigos e Porteiro do Convento, cargo este considerado importante porque pela comunicação com as pessoas permitia fazer um grande apostolado, ouvindo e aconselhando a todos. Foi confessor estimado e muitas vezes, quando era chamado, ia a pé mesmo aos lugares distantes. Em 1769-70 foi designado Confessor de um Recolhimento de piedosas mulheres, as “Recolhidas de Santa Teresa” em São Paulo. Neste Recolhimento, encontrou Irmã Helena Maria do Espírito Santo, religiosa de profunda oração e grande penitência, observante da vida comum, que afirmava ter visões pelas quais Jesus lhe pedia para fundar um novo Recolhimento. Frei Galvão, como confessor, ouviu e estudou tais mensagens e solicitou o parecer de pessoas sábias e esclarecidas, que reconheceram tais visões como válidas.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó querido Santo Antônio de Santana Galvão, homem forte e valente, dai-nos a graça de enfrentarmos com coragem todas as tribulações desta vida, imitando o vosso exemplo para combatermos o bom combate sem nos acovardarmos.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String quintoDia = "A data oficial da fundação do novo Recolhimento é 2 de fevereiro de 1774. Irmã Helena queria modelar o Recolhimento segundo a Ordem Carmelitana, mas o Bispo de São Paulo, franciscano e defensor da Imaculada, quis que fosse segundo a regra das Concepcionistas aprovadas pelo Papa Júlio II, em 1511. A fundação passou a se chamar “Recolhimento de Nossa Senhora da Conceição da Divina Providência” e Frei Galvão, seu fundador. No dia 23 de fevereiro de 1775 morre a Irmã Helena. Durante quatorze anos (1774-1788) Frei Galvão cuidou da construção do Recolhimento. Outros quatorze anos dedicou-se à construção da Igreja, inaugurada em 15 de agosto de 1802. A obra tornou-se por decisão da UNESCO “Patrimônio Cultural da Humanidade”.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó incansável Santo Antônio de Santana Galvão, tu que tinhas nesta terra dons extraordinários, dai-nos a graça sabermos administrar todos os dons que recebemos de Deus, imitando o vosso exemplo para que nossas obras ajudem o próximo e sejam agradáveis a Deus.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String sextoDia = "Frei Galvão, além da construção e dos encargos especiais dentro e fora da Ordem Franciscana, deu muita atenção e o melhor de suas forças à formação das Recolhidas. Para elas escreveu um regulamento ou Estatuto, excelente guia de vida interior e de disciplina religiosa. O Estatuto é o principal escrito, o que melhor manifesta a personalidade do Servo de Deus. Então o Bispo de São Paulo acrescentou ao Estatuto a permissão para as Recolhidas emitirem os votos enquanto permanecessem na casa religiosa.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó fidelíssimo Santo Antônio de Santana Galvão, homem de fé exemplar, dai-nos a graça de crescermos de fé em fé, imitando o vosso exemplo para que nossa fé não seja um sentimentalismo fútil, mas que se converta em obras agradáveis a Deus.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String setimoDia = "Em 1781, foi nomeado Mestre do noviciado de Macacu, Rio de Janeiro. O Bispo, porém, que o queria em São Paulo, não fez chegar a ele a carta do Superior Provincial. Frei Galvão foi nomeado Guardião do Convento de S. Francisco em São Paulo, em 1798 e reeleito em 1801. Tornou-se Guardião sem deixar a direção espiritual das Recolhidas. Em 1811, a pedido do Bispo de São Paulo, fundou o Recolhimento de Santa Clara em Sorocaba, no Estado de S. Paulo. Aí permaneceu onze meses para organizar a comunidade e dirigir os trabalhos iniciais da construção da Casa.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó piedoso Santo Antônio de Santana Galvão, Santo da caridade, dai-nos a graça de perseverarmos em nossas obras, imitando o vosso exemplo para que elas deem muitos frutos e despertem nos homens desejo de seguir os passos de nosso Senhor.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String oitavoDia = "Frei Galvão voltou para São Paulo, onde permaneceu 10 anos, no Recolhimento da Luz. Durante sua doença, Frei Antônio passou a morar num “quartinho” atrás do Tabernáculo, no fundo da Igreja, graças a insistências das religiosas, que desejavam prestar-lhe algum alívio e conforto. Por causa do imenso amor e caridade de seu Servo, Deus o agraciou com diversos dons, dos quais jamais serviu-se em interesse próprio, ao contrário, sempre os colocou a serviço da misericórdia divina. Todos os casos narrados foram devidamente comprovados por documentos. São eles: Bilocação (estar em mais de um lugar ao mesmo tempo), telepatia (transmissão ou comunicação de pensamento e sensações, a distância entre duas ou mais pessoas), premonição (sensação ou advertência antecipada do que vai acontecer), clarividência (vê o que está para acontecer), levitação (erguer-se acima do solo) e telepercepção (adquirir conhecimento de fatos ocorridos a grandes distâncias).\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó clemente Santo Antônio de Santana Galvão, exemplo de obediência à nosso Senhor, dai-nos a graça de procuramos antes agradar a Deus do que aos homens, imitando o vosso exemplo para que nunca nos envergonhemos de nossa fé por causa do respeito humano.\n"
+            + "\n"
+            + "Orações finais para todos os dias.";
+
+    private String nonoDia = "Antônio de Sant’Ana Galvão morreu no dia 23 de dezembro de 1822. A pedido das religiosas e do povo, foi sepultado na Igreja do Recolhimento que ele mesmo construíra. O seu túmulo é lugar de peregrinação dos fiéis, que pedem e agradecem graças por intercessão do “homem da paz e da caridade”, e fundador do Recolhimento de Nossa Senhora da Luz. Frei Antônio de Sant’Ana Galvão foi Beatificado pelo Papa São João Paulo II, no dia 25 de outubro de 1998. Foi Canonizado pelo Papa Bento XVI, durante sua visita ao Brasil, no dia 11 de maio de 2007.\n"
+            + "\n"
+            + "Oração do dia\n"
+            + "Ó castíssimo Santo Antônio de Santana Galvão, espelho de virtudes para o nosso Brasil, dai-nos a graça de vivermos a pobreza, a obediência e a castidade, imitando o vosso exemplo para que cheios do espírito santo possamos um dia gozar das alegrias da pátria eterna.\n"
+            + "\n"
+            + "Orações finais para todos os dias";
+}
